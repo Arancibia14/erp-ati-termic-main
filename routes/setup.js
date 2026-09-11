daebd9bc8578b9aabccd5edd071a44fe787c5c20
martincduc <martinluissaez@gmail.com>
2026-09-10 22:45:15 -0300

    Corrección modificaciones de contrato solo para administrador

--- routes/setup.js
+++ routes/setup.js
@@ -6,23 +6,24 @@ const {
   crearGuiaDespacho, crearContratoLaboral, actualizarContratoLaboral, eliminarContratoLaboral,
   actualizarCoordenadasProyecto
 } = require('../controllers/setupController');
-const { verifyToken } = require('../middleware/auth');
+const { verifyToken, requireAdmin } = require('../middleware/auth');
 
 router.get('/estados',           verifyToken, getEstados);
 router.get('/especialidades',    verifyToken, getEspecialidades);
 router.get('/proyectos',         verifyToken, getProyectos);
 router.get('/trabajadores',      verifyToken, getTrabajadores);
 router.get('/ordenes',           verifyToken, getOrdenes);
-router.get('/contratos',         verifyToken, getContratos);
+// Los contratos incluyen sueldos: solo el administrador los gestiona
+router.get('/contratos',         verifyToken, requireAdmin, getContratos);
 router.post('/proyecto',         verifyToken, crearProyecto);
 router.post('/proveedor',        verifyToken, crearProveedor);
 router.post('/trabajador',       verifyToken, crearTrabajador);
 router.post('/hito',             verifyToken, crearHito);
 router.post('/solicitud-material', verifyToken, crearSolicitudMaterial);
 router.post('/guia-despacho',    verifyToken, crearGuiaDespacho);
-router.post('/contrato',         verifyToken, crearContratoLaboral);
+router.post('/contrato',         verifyToken, requireAdmin, crearContratoLaboral);
 router.put('/proyecto/:codigo/coordenadas', verifyToken, actualizarCoordenadasProyecto);
-router.put('/contrato/:id',      verifyToken, actualizarContratoLaboral);
-router.delete('/contrato/:id',   verifyToken, eliminarContratoLaboral);
+router.put('/contrato/:id',      verifyToken, requireAdmin, actualizarContratoLaboral);
+router.delete('/contrato/:id',   verifyToken, requireAdmin, eliminarContratoLaboral);
 
 module.exports = router;
